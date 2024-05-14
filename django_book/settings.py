7fccccb241971df3ac696fceed9a7d555ab2c7d3
KoPiyt <108018496+KoPiyt@users.noreply.github.com>
2024-05-15 00:38:13 +0900

    [add]modelsとsettingsに認識 2024/05/15

--- django_book/settings.py
+++ django_book/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "learning_logs", # 自分のアプリ
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
